f3f1fd5154a358cbbf3e85a7f1a7930693a3a4bf
William Roe <git@wjlr.org.uk>
2013-08-12 21:06:15 +0100

    Correct github url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject onyx-dragon "0.1.0-SNAPSHOT"
   :description "A basic script for checking a CloudFront distribution for invalidations forever"
-  :url "https://github.com/digital-science/onyx_dragon"
+  :url "https://github.com/digital-science/onyx-dragon"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
